bb451ccef7d32fa4c4be9e1a521057830a36ea93
Fanshaoliu <549725406@qq.com>
2021-06-12 18:58:29 +0800

    add new hiro project

--- run_shell/HIRO_SafeGym.sh
+++ run_shell/HIRO_SafeGym.sh
@@ -1,3 +1,3 @@
 echo "`date '+%Y%m%d %H:%M:%S'`"
-python -u clean_hiro/hiro_pytorch/main_sg.py --train --model_save_freq 100 --num_episode 500 --steps_per_epoch 30000 --env Safexp-PointGoal0-v0 --exp_name "Safexp-PointGoal0-v0-para0"
+python -u clean_hiro/hiro_pytorch/main_sg.py --train --model_save_freq 1000 --num_episode 10000 --steps_per_epoch 30000 --env Safexp-PointGoal0-v0 --exp_name "Safexp-PointGoal0-v0-para0"
 
