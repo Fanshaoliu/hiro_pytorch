7717b52a185e3a433dc66bded25263a562d3d4a9
Fanshaoliu <549725406@qq.com>
2021-06-12 14:22:46 +0800

    first commit in safe-rl

--- train.sh
+++ train.sh
@@ -1,5 +1,11 @@
-echo "`date '+%Y%m%d %H:%M:%S'`"
-nohup sh run_shell/HIRO_AntMaze.sh > shell_output/HIRO_AntMaze.txt 2>&1 &
+# 1.HIRO Ant, baseline of HRL
+#echo "`date '+%Y%m%d %H:%M:%S'`"
+#nohup sh run_shell/HIRO_AntMaze.sh > shell_output/HIRO_AntMaze.txt 2>&1 &
+#
+#echo "`date '+%Y%m%d %H:%M:%S'`"
+#nohup sh run_shell/HIRO_AntPush.sh > shell_output/HIRO_AntPush.txt 2>&1 &
 
+# 2.HIRO SafeGym, baseline of HRL
+echo "HIRO SafeGym, baseline of HRL"
 echo "`date '+%Y%m%d %H:%M:%S'`"
-nohup sh run_shell/HIRO_AntPush.sh > shell_output/HIRO_AntPush.txt 2>&1 &
+nohup sh run_shell/HIRO_SafeGym.sh > shell_output/HIRO_SafeGym.txt 2>&1 &
