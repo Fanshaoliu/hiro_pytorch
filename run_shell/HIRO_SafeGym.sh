208e3a0001bd245170bd83718a161ca836ce3e52
Fanshaoliu <549725406@qq.com>
2021-06-12 16:10:38 +0800

    update training hyperpara of HIRO for safegym goal

--- run_shell/HIRO_SafeGym.sh
+++ run_shell/HIRO_SafeGym.sh
@@ -1,3 +1,3 @@
 echo "`date '+%Y%m%d %H:%M:%S'`"
-python -u clean_hiro/hiro_pytorch/main_sg.py --train --env Safexp-PointGoal0-v0 --exp_name "Safexp-PointGoal0-v0 with origin HIRO"
+python -u clean_hiro/hiro_pytorch/main_sg.py --train --model_save_freq 100 --num_episode 500 --steps_per_epoch 30000 --env Safexp-PointGoal0-v0 --exp_name "Safexp-PointGoal0-v0-para0"
 
--- train.sh
+++ train.sh
@@ -1,7 +1,9 @@
 # 1.HIRO Ant, baseline of HRL
+#echo "HIRO AntMaze, baseline of HRL"
 #echo "`date '+%Y%m%d %H:%M:%S'`"
 #nohup sh run_shell/HIRO_AntMaze.sh > shell_output/HIRO_AntMaze.txt 2>&1 &
 #
+#echo "HIRO AntPush, baseline of HRL"
 #echo "`date '+%Y%m%d %H:%M:%S'`"
 #nohup sh run_shell/HIRO_AntPush.sh > shell_output/HIRO_AntPush.txt 2>&1 &
 
